e31f6ddc9d5c3edb8fd0eb918d4f6cd516fbac60
newrecha <165303543+newrecha@users.noreply.github.com>
2024-09-23 01:53:27 +0800

    1

---
+++ ytlb.php
@@ -0,0 +1,89 @@
+
+{
+    "class": [
+        {
+            "type_name": "定义栏目",
+            "type_id": "定义栏目",
+            "parse": 0,
+            "jx": 0
+        },
+        {
+            "type_name": "流行音樂",
+            "type_id": "流行音樂",
+            "parse": 0,
+            "jx": 0
+        },
+        {
+            "type_name": "音樂排行",
+            "type_id": "音樂排行",
+            "parse": 0,
+            "jx": 0
+        },
+        {
+            "type_name": "喜劇",
+            "type_id": "喜劇",
+            "parse": 0,
+            "jx": 0
+        },
+        {
+            "type_name": "我的歌單",
+            "type_id": "我的歌單",
+            "parse": 0,
+            "jx": 0
+        },
+        {
+            "type_name": "穿越短剧",
+            "type_id": "穿越短剧",
+            "parse": 0,
+            "jx": 0
+        },
+        {
+            "type_name": "搖滾音樂",
+            "type_id": "搖滾音樂",
+            "parse": 0,
+            "jx": 0
+        },
+        {
+            "type_name": "有声小说",
+            "type_id": "有声小说",
+            "parse": 0,
+            "jx": 0
+        },
+        {
+            "type_name": "短剧",
+            "type_id": "短剧",
+            "parse": 0,
+            "jx": 0
+        },
+        {
+            "type_name": "動畫",
+            "type_id": "動畫",
+            "parse": 0,
+            "jx": 0
+        },
+        {
+            "type_name": "沙雕穿越",
+            "type_id": "沙雕穿越",
+            "parse": 0,
+            "jx": 0
+        },
+        {
+            "type_name": "韓劇",
+            "type_id": "韓劇",
+            "parse": 0,
+            "jx": 0
+        },
+        {
+            "type_name": "陸劇",
+            "type_id": "陸劇",
+            "parse": 0,
+            "jx": 0
+        },
+        {
+            "type_name": "感人電影",
+            "type_id": "感人電影",
+            "parse": 0,
+            "jx": 0
+        }
+    ]
+}
